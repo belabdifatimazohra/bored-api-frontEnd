77601c040aea5d4d24be4b44977eb240605e60eb
belabdifatimazohra <fatima.zohra.belabdi@gmail.com>
2022-03-19 14:21:21 +0100

    delete from favorite

--- src/redux/reducers/favoriteActivitiesReducer.js
+++ src/redux/reducers/favoriteActivitiesReducer.js
@@ -16,12 +16,10 @@ export const favoriteActivitiesReducer = (state = intialState, action) => {
       return { favoriteActivities: [...state.favoriteActivities, action.payload] };
     case "DELETE":
       // Filter the list # to the one deleted and return it as the new state of the favorit activities
-      console.log(state.favoriteActivities);
-      newState = JSON.parse(localStorage.getItem("favorites")).filter(
+      newState = state.favoriteActivities.filter(
         (activity) => activity.id !== action.payload.id);
-      console.log(newState);
-      localStorage.setItem("favorites", JSON.stringify(newState))
-      return { favoriteActivities: [newState, action.payload] };
+      localStorage.setItem("favorites", JSON.stringify([...newState]))
+      return { favoriteActivities: [...newState] };
     case "EMPTY":
       return {
         favoriteActivities: [],
